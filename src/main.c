3923dfb5c61ef9e711b43b66a322777a8dd16e8f
Florian Bach <Leseratte10@vodafone.de>
2021-05-23 18:05:07 +0200

    Stub original timing / position code

--- src/main.c
+++ src/main.c
@@ -116,15 +116,12 @@ void loadCodes() {
 	// End Race on Command (by CLF78)
 	directWriteBranch(No5LimitHook, TimerEnd, false);
 
-	// Finish Position/Points Updater (by CLF78)
+	// Finish Position/Points Updater (by CLF78 and Leseratte)
 	directWrite32(FinishPoints, 0x48000014);
-	directWriteBranch(FixPositionsHook, FixPositionsHelper, false);
-	directWriteNop(FixPositions2);
-	directWriteNop(FixPositions3);
-	directWriteBranch(FixPositions4Hook, FixPositions4, true);
-	directWriteBranch(FixPositions5Hook, FixPositions5Helper, false);
-	directWrite16(FixPositions6, tempVal16);
-	directWriteBranch(FixFanfareHook, FixFanfare, true);
+	directWriteNop(StopUpdatingTimerMin);
+	directWriteNop(StopUpdatingTimerSec);
+	directWriteNop(StopUpdatingTimerMs);
+	directWriteBlr(StopUpdatingPosTracker);
 
 	// Force 150cc (by XeR, modified by CLF78)
 	directWriteBranch(ForceCCHook, ForceCC, true);
