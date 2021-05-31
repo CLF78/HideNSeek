1d7505fddb8c8b0cc05fa9b0a689743850ffe6dd
H1dd3nM1nd <twomp021@gmail.com>
2021-05-31 16:31:09 +0200

    Revert "Allow Seekers to clip through each other"
    This reverts commit 6a150dc0ec5d9b7eab8000d70f739d96891cd5db.

--- src/main/noclip.c
+++ src/main/noclip.c
@@ -1,6 +1,6 @@
 #include <hidenseek.h>
 #include <player.h>
 
-bool NoClip(PlayerHolderPlayer* player, PlayerHolderPlayer* player2) {
-	return (HideNSeekData.players[player->playerPointers->params->playerId].isSeeker == HideNSeekData.players[player2->playerPointers->params->playerId].isSeeker);
+bool NoClip(PlayerHolderPlayer* player) {
+	return (!HideNSeekData.players[player->playerPointers->params->playerId].isSeeker);
 }
