4d66159dde273c26d2d50375376aa93df95f7ef9
H1dd3nM1nd <twomp021@gmail.com>
2021-06-03 15:51:30 +0200

    Bump version

--- src/main.c
+++ src/main.c
@@ -156,7 +156,7 @@ void loadCodes() {
 	directWriteBranch(GuestSendHook, GuestSend, false);
 	directWriteBranch(HostCheckHook, HostCheck, true);
 	directWriteBranch(HostCheckHelperHook, HostCheckHelper, true);
-	directWrite8(Version, 3);
+	directWrite8(Version, 4);
 
 	// Improved Position Interpolation (by CLF78, Leseratte & stebler)
 	directWriteNop(ForceInterpolation);
