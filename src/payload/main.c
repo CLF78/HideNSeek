543e7f4a738a5e360e692cfbcc3c420cf231dce9
CLF78 <29682335+CLF78@users.noreply.github.com>
2023-05-22 11:10:19 +0200

    Fix version check broken by Wiimmfi payload update

--- src/payload/main.c
+++ src/payload/main.c
@@ -141,7 +141,7 @@ void loadCodes() {
 
 	// Host Version Check (by CLF78 & Seeky)
 	directWriteBranch(GuestSendHook, GuestSend, false);
-	directWriteBranch(HostCheckHook, HostCheck, true);
+	directWriteBranch(HostCheckHook, HostCheck, false);
 	directWriteBranch(HostCheckHelperHook, HostCheckHelper, true);
 	directWrite8(Version, 7);
 
